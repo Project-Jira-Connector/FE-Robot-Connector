9ab44d4d3049a95391d0b46d503fb817ccea50ae
farreldavaa <farrel.dava@binus.ac.id>
2023-04-14 08:50:55 +0700

    push baru

--- src/pages/RobotInput.rs
+++ src/pages/RobotInput.rs
@@ -614,10 +614,11 @@ impl Component for RobotInput {
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">{"Modal title"}</h5>
+                                <h5 class="modal-title" id="exampleModalLabel">{"Save Changes"}</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
+                                {"Have you make sure due to field changed?"}
                             </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{"Close"}</button>
